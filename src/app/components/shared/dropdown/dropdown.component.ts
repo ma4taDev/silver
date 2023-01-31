a1c6781075e9789cb8ee0f4433d6fe588f2e84cb
Masoud <masoud.tahmasebi@digitalspital.de>
2023-01-31 01:25:13 +0100

    feat: added new bootstrap and also desing

---
+++ src/app/components/shared/dropdown/dropdown.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-dropdown',
+  templateUrl: './dropdown.component.html',
+  styleUrls: ['./dropdown.component.scss']
+})
+export class DropdownComponent {
+
+  @Input() title: string = '';
+  @Input() options = '';
+
+}
